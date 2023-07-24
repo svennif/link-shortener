886781f33a12aad27bbb2fd953a892635bf6c7d3
Sven Ingar Frantzen <sven.ingar@gmail.com>
2023-07-24 15:58:58 +0200

    added axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import axios from 'axios'
 
 function App() {
+  // TODO Function for shortening the URL when the button is clicked
+
+  // TODO Import bit.ly API
+  
   return (
-    <div>App</div>
+    <>
+    <label htmlFor="long-url">Insert your URL</label>
+    <input type="text" name="long-url" id="long-url" />
+    <button type="button">Shorten</button>
+    </>
   )
 }
 
