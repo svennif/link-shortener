a57f43ca5c8c441d1806f3c13698fc96b5d0cb5a
Sven Ingar Frantzen <sven.ingar@gmail.com>
2023-07-26 00:03:58 +0200

    Changed from bitly to shrtco.de

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useState } from 'react';
+import axios from 'axios';
+import ClipboardJS from 'clipboard';
 
 function App() {
-  // TODO Function for shortening the URL when the button is clicked
+  new ClipboardJS('.copy-short-link');
+  const [input, setInput] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+  const [copy, setCopy] = useState(false);
 
-  // TODO Import bit.ly API
-  
+  async function shortenUrl() {
+    try {
+      const res = await axios.get(
+        `https://api.shrtco.de/v2/shorten?url=${input}`
+      );
+      setShortUrl(res.data.result.full_short_link);
+    } catch (error) {
+      console.error();
+    }
+  }
+
   return (
     <>
-    <label htmlFor="long-url">Insert your URL</label>
-    <input type="text" name="long-url" id="long-url" />
-    <button type="button">Shorten</button>
+      <form onSubmit={(e) => { e.preventDefault(); shortenUrl(); }}>
+        <label htmlFor="long-url">Insert your URL</label>
+        <input
+          type="text"
+          name="long-url"
+          id="long-url"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
+        <button type="submit">Shorten</button>
+      </form>
+
+      {shortUrl && (
+        <div>
+          <input
+            type="text"
+            id='shortened-link'
+            className="shortened-link"
+            value={shortUrl}
+            readOnly
+          /></div>
+      )}
+      <button type='button' className="copy-short-link" data-clipboard-target="#shortened-link" onClick={() => setCopy(true)}>
+        {copy ? "Copied" : "Copy"}
+      </button>
     </>
   )
 }
 
-export default App
+export default App;
